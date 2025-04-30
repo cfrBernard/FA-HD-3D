3ce3b4337884032873ae0b1c06a04bdf9b15982a
C. Bernard <cfr.bernard@yahoo.com>
2025-04-30 06:00:10 +0200

    chore(UI) devConsole setup

--- Assets/Core/Scripts/Tools/DevConsole/DevConsole.cs
+++ Assets/Core/Scripts/Tools/DevConsole/DevConsole.cs
@@ -1,6 +1,33 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class DevConsole : MonoBehaviour
 {
+    public static DevConsole Instance { get; private set; }
 
+    private List<string> commandHistory = new List<string>();
+    private bool showConsole = false;
+    
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Update()
+    {
+
+    }
+
+    private void RunCommand(string cmd)
+    {
+
+    }
 }
--- Assets/Features/UI/Scripts/SettingsMenu/Managers/TabManager.cs
+++ Assets/Features/UI/Scripts/SettingsMenu/Managers/TabManager.cs
@@ -1,7 +1,6 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.InputSystem;
-using UnityEngine.EventSystems;
 
 public class TabManager : MonoBehaviour
 {
